fae9b22e1b32ce06a755f63a3c6dd2b9862297f0
Ziad El-Habashy <ziad2402269@miuegypt.edu.eg>
2026-05-30 22:36:50 +0300

    Add DNS configuration for MongoDB connection to improve SRV lookup reliability

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,8 @@
+const dns = require('dns');
 const mongoose = require('mongoose');
+
+// Windows/local DNS often fails SRV lookups for mongodb+srv:// URIs
+dns.setServers(['8.8.8.8', '1.1.1.1']);
 
 const connectDB = async () => {
   try {
